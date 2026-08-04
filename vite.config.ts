bae74ae6a9a07e383f7f35c7c48c491bbca21489
Liana Jomaa <lianajomaa95@gmail.com>
2026-08-05 00:45:10 +0300

    Restore Tailwind plugin, add separate vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  },
+  plugins: [react(), tailwindcss()],
 })
